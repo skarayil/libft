51d7b3fd9e0cdb24d48193df1b1beb97970487fb
Sude Naz Karayildirim <skarayil@k2m09s05.42kocaeli.com.tr>
2025-06-04 12:56:47 +0300

    sude naz karayıldırım

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,27 +6,14 @@
 /*   By: skarayil <skarayil@student.42kocaeli>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/27 12:17:24 by skarayil          #+#    #+#             */
-/*   Updated: 2025/05/31 12:26:35 by skarayil         ###   ########.fr       */
+/*   Updated: 2025/06/04 12:55:59 by skarayil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stddef.h>
+#include "libft.h"
 
 void	*ft_memcpy(void *dest, const void *src, size_t n)
 {
-	size_t			index;
-	unsigned char	*s;
-	unsigned char	*d;
-
-	if (src == NULL || dest == NULL)
-		return (NULL);
-	s = (unsigned char *)src;
-	d = (unsigned char *)dest;
-	index = 0;
-	while (index < n)
-	{
-		d[index] = s[index];
-		index++;
-	}
+	ft_memmove(dest, src, n);
 	return (dest);
 }

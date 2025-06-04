07b7725fa8af29c3a9b6d3c2209555d3e200961d
Sude Naz Karayildirim <skarayil@k2m09s05.42kocaeli.com.tr>
2025-06-04 12:18:54 +0300

    sude naz karayıldırım

--- ft_strdup.c
+++ ft_strdup.c
@@ -6,27 +6,27 @@
 /*   By: skarayil <skarayil@student.42kocaeli>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/27 15:35:21 by skarayil          #+#    #+#             */
-/*   Updated: 2025/05/31 12:31:10 by skarayil         ###   ########.fr       */
+/*   Updated: 2025/06/03 17:19:21 by skarayil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 
-char	*ft_strdup(char *src)
+char	*ft_strdup(char *s)
 {
 	int		index;
 	char	*new_str;
 
 	index = 0;
-	while (src[index])
+	while (s[index])
 		index++;
 	new_str = (char *)malloc(sizeof(char) * (index + 1));
 	if (!new_str)
 		return (NULL);
 	index = 0;
-	while (src[index])
+	while (s[index])
 	{
-		new_str[index] = src[index];
+		new_str[index] = s[index];
 		index++;
 	}
 	new_str[index] = '\0';

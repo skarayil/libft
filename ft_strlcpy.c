8c7b0d6b9399a4f6dd695b2a2050798c9eb94805
Sude Naz Karayıldırım <sudekarayildirim@gmail.com>
2026-01-10 23:02:28 +0300

    sude naz karayıldırım
    Removed unnecessary include for stddef.h.

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -10,7 +10,6 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stddef.h>
 #include "libft.h"
 
 size_t	ft_strlcpy(char *dst, const char *src, size_t size)
